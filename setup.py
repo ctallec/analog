5fdc3d6b460e6f06d342087e4ca643b4b6d78072
ctallec <corentinxtallec@gmail.com>
2019-01-24 18:57:18 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='analog',
+      version='0.1',
+      description='Log Analysis python tool.',
+      url='',
+      author='Leonard Blier',
+      author_email='',
+      license='MIT',
+      packages=['analog'],
+      zip_safe=False)
